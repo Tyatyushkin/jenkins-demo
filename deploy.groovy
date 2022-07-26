4f92e162350a3adc9262caa24db36c5ec7cf3cbc
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2022-07-26 06:28:44 +0300

    modify repo state

--- deploy.groovy
+++ deploy.groovy
@@ -6,7 +6,13 @@ pipeline {
             steps {
                 sshagent(credentials: ['repo']) {
                     echo 'Clone repo'
+                    sh: 'git-add -l'
+                    sh '''
+                        [ -d ~/.ssh ] || mkdir ~/.ssh && chmod 0700 ~/.ssh
+                        ssh-keyscan -t rsa,dsa github.com >> ~/.ssh/known_hosts
+                         '''
                     git([url: 'git@github.com:Tyatyushkin/jenkins-dockerfile.git', branch: 'main'])
+
                 }
             }
         }
