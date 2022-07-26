f27b9e58393d7a355d721b680e8895531cc0bea1
Maxim Tyatyushkin <maxim.tyatyushkin@gmail.com>
2022-07-26 06:54:06 +0300

    add docker build check

--- deploy.groovy
+++ deploy.groovy
@@ -12,7 +12,7 @@ pipeline {
         stage('Test') {
             steps {
                 echo 'Testing..'
-                sh 'ls -a'
+                sh 'docker build -t test'
             }
         }
         stage('Deploy') {
